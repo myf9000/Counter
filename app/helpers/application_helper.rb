6c8f7a3d26936b89b00fc8c0e1565bcc458ae69b
myf900 <myf9000@vp.pl>
2015-11-14 20:56:49 +0100

    Add tweet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 	def set_title_of_page(page_title="")
-		add = "Count Your's TxT"
+		add = "Count text"
 		if page_title.empty?
 			page_title = add
 		else
-			page_title = page_title+ " | " +add
+			page_title = add+": "+page_title
 		end
 	end
 end
